34a572c3d585f74aa601d5fa379a3d156a3f1866
Nicholas Wilcox <nwilcox@linicks.dev>
2022-05-16 17:18:36 -0500

    Add ability to show/hide different output rows based on form controls and query parameters

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const functionNames = [
-	'toString',
-	'toDateString',
-	'toTimeString',
-	'toISOString',
-	'toUTCString',
-	'toLocaleString',
-	'toLocaleDateString',
-	'toLocaleTimeString',
-];
-
 const omitFromDateOptions = [
 	"timeStyle",
 ];
@@ -17,8 +6,12 @@ const omitFromTimeOptions = [
 	"dateStyle",
 ];
 
+const params = new URLSearchParams(window.location.search);
 const inputDateSelector = document.querySelector('#inputDate');
 const useNowCheckbox = document.querySelector('#useNow');
+const inputDateForm = document.querySelector('#inputDateForm');
+const outputVisibilityForm = document.querySelector('#outputVisibilityForm');
+const outputContainer = document.querySelector('#output');
 
 let inputDate;
 let updateDateInterval;
@@ -27,8 +20,7 @@ let formatOptions = {};
 function computeInputDate() {
 	useNowCheckbox.checked = false;
 	clearInterval(updateDateInterval);
-	const form = document.querySelector('#dateFormatSelectorForm');
-	const formData = new FormData(form);
+	const formData = new FormData(inputDateForm);
 	if (formData.get('inputDate')) {
 		inputDate = new Date(formData.get('inputDate'));
 		updateOutput();
@@ -70,17 +62,50 @@ function setInputDateToNow() {
 	updateOutput();
 }
 
-function onRadioInput(event) {
+function onFormatOptionRadioInput(event) {
 	formatOptions = Object.assign(formatOptions, { [event.target.name]: event.target.value });
 	updateOutput();
-	console.log(formatOptions);
+}
+
+function updateOutputVisibility() {
+	const showList = params.getAll('s');
+	let shouldHide;
+	if (showList.length === 0) {
+		shouldHide = code => false;
+	} else {
+		shouldHide = code => !showList.includes(code);
+	}
+
+	const formData = new FormData(outputVisibilityForm);
+	const hasSetShowNonLocale = formData.get('showNonLocale') !== null;
+	const hasSetShowRedundant = formData.get('showRedundant') !== null;
+	const hasSetEither = hasSetShowNonLocale || hasSetShowRedundant;
+	const hideNonLocale = (formData.get('showNonLocale') ?? 'true') === 'false';
+	const hideRedundant = (formData.get('showRedundant') ?? 'true') === 'false';
+	const getOutputRow = code => document.querySelector(`.dateStringOutputRow[row-code=${code}]`);
+
+	[
+		['ts', hasSetShowNonLocale ? hideNonLocale : shouldHide('ts')],
+		['tds', hasSetEither ? hideNonLocale || hideRedundant : shouldHide('tds')],
+		['tts', hasSetEither ? hideNonLocale || hideRedundant : shouldHide('tts')],
+		['tis', hasSetShowNonLocale ? hideNonLocale : shouldHide('tis')],
+		['tus', hasSetShowNonLocale ? hideNonLocale : shouldHide('tus')],
+		['tls', shouldHide('tls')],
+		['tlds', hasSetShowRedundant ? hideRedundant : shouldHide('tlds')],
+		['tlts', hasSetShowRedundant ? hideRedundant : shouldHide('tlts')],
+	].forEach(([code, value]) => getOutputRow(code).classList.toggle('hidden', value));
 }
 
 useNowCheckbox.addEventListener('change', toggleUseNow);
 inputDateSelector.addEventListener('input', computeInputDate);
-document.querySelectorAll('input[type=radio]').forEach(radio => radio.addEventListener('input', onRadioInput));
+document.querySelectorAll('#outputVisibilityForm input[type=radio]')
+	.forEach(radio => radio.addEventListener('input', updateOutputVisibility));
+document.querySelectorAll('#formatOptionsForm input[type=radio]')
+	.forEach(radio => radio.addEventListener('input', onFormatOptionRadioInput));
 
 // Initialize values
 inputDate = new Date();
 inputDateSelector.value = inputDate.toISOString().substring(0, 19);
 updateOutput();
+updateOutputVisibility();
+outputContainer.classList.toggle('hidden', false);
