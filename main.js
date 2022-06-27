cf499b07f484628247ad2925a7eab548612e7033
Nicholas Wilcox <nwilcox@linicks.dev>
2022-06-27 01:04:04 -0500

    Add some better styles and inputs for locales and time zones

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const output = document.querySelector('#output');
-const inputDateSelector = document.querySelector('#inputDate');
-const useNowCheckbox = document.querySelector('#useNow');
-const inputDateForm = document.querySelector('#inputDateForm');
+const output = document.getElementById('output');
+const inputDateSelector = document.getElementById('inputDate');
+const useNowCheckbox = document.getElementById('useNow');
+const inputLocale = document.getElementById('locale');
+const timeZoneInput = document.getElementById('timeZone');
+const inputDateForm = document.getElementById('inputDateForm');
 
 let inputDate;
 let updateDateInterval;
+let locales;
+let timeZone;
 
 function computeInputDate() {
   useNowCheckbox.checked = false;
@@ -17,7 +21,7 @@ function computeInputDate() {
 }
 
 function updateOutput() {
-  output.innerText = inputDate.toLocaleString([], formatOptions);
+  output.innerText = inputDate.toLocaleString(locales, Object.assign({timeZone, ...formatOptions}));
 }
 
 function toggleUseCurrentTime() {
@@ -34,10 +38,54 @@ function setInputDateToNow() {
   updateOutput();
 }
 
+function updateLocale() {
+  try {
+    if (inputLocale.value) {
+      const inputLocales = inputLocale.value?.split(/,\s*/).filter(s => s !== '');
+      const supportedLocales = Intl.DateTimeFormat.supportedLocalesOf(inputLocales);
+      if (supportedLocales.length) {
+        locales = supportedLocales;
+        inputLocale.classList.remove('error');
+      }
+    } else {
+      inputLocale.classList.remove('error');
+    }
+  } catch {
+    inputLocale.classList.add('error');
+  }
+  updateOutput();
+}
+
+function updateTimeZone() {
+  try {
+    if (timeZoneInput.value) {
+      Intl.DateTimeFormat(undefined, {timeZone: timeZoneInput.value});
+      timeZone = timeZoneInput.value;
+      timeZoneInput.classList.remove('error');
+    } else {
+      timeZoneInput.classList.remove('error');
+    }
+  } catch {
+    timeZoneInput.classList.add('error');
+  }
+  updateOutput();
+}
+
+function preventSubmit(event) {
+  if (event.code === 'Enter') {
+    event.preventDefault();
+  }
+}
+
 useNowCheckbox.addEventListener('change', toggleUseCurrentTime);
 inputDateSelector.addEventListener('input', computeInputDate);
+inputLocale.addEventListener('input', updateLocale);
+inputLocale.addEventListener('keydown', preventSubmit);
+timeZoneInput.addEventListener('input', updateTimeZone);
+timeZoneInput.addEventListener('keydown', preventSubmit);
 
 // Initialize values
 inputDate = new Date();
 inputDateSelector.value = inputDate.toISOString().substring(0, 19);
+inputLocale.setAttribute('placeholder', `(e.g. "${navigator.languages[0]}")`);
 updateOutput();
