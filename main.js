426e170269894c38d15f0997c673807c0a0d3f21
Nicholas Wilcox <nwilcox@linicks.dev>
2024-04-25 14:40:47 -0500

    Reformat

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
 const FORMAT_PROPERTIES = [
-  'weekday',
-  'era',
-  'year',
-  'month',
-  'day',
-  'dayPeriod',
-  'hour',
-  'minute',
-  'second',
-  'fractionalSecondDigits',
-  'timeZoneName',
+  "weekday",
+  "era",
+  "year",
+  "month",
+  "day",
+  "dayPeriod",
+  "hour",
+  "minute",
+  "second",
+  "fractionalSecondDigits",
+  "timeZoneName",
 ];
 
-const STYLE_PROPERTIES = [
-  'dateStyle',
-  'timeStyle',
-];
+const STYLE_PROPERTIES = ["dateStyle", "timeStyle"];
 
 // Element handles
-const dateInput = document.getElementById('dateInput');
-const useNowCheckbox = document.getElementById('useNow');
-const localesInput = document.getElementById('localesInput');
-const timeZoneInput = document.getElementById('timeZoneInput');
-const output = document.getElementById('output');
-const optionsForm = document.getElementById('formatOptionsForm');
+const dateInput = document.getElementById("dateInput");
+const useNowCheckbox = document.getElementById("useNow");
+const localesInput = document.getElementById("localesInput");
+const timeZoneInput = document.getElementById("timeZoneInput");
+const output = document.getElementById("output");
+const optionsForm = document.getElementById("formatOptionsForm");
 
 let date;
 let updateDateInterval;
@@ -37,7 +34,7 @@ function setFormatOption(name, value) {
 
 /** Unsets any given format options */
 function clearProperties(...props) {
-  props.forEach(key => {
+  props.forEach((key) => {
     delete formatOptions[key];
     const rg = document.querySelector(`radio-group[name="${key}"]`);
     rg?.clear();
@@ -45,7 +42,10 @@ function clearProperties(...props) {
 }
 
 function updateOutput() {
-  output.innerText = date.toLocaleString(locales, { timeZone, ...formatOptions });
+  output.innerText = date.toLocaleString(locales, {
+    timeZone,
+    ...formatOptions,
+  });
 }
 
 function setInputDateToNow() {
@@ -54,13 +54,13 @@ function setInputDateToNow() {
 }
 
 function preventSubmitOnEnter(event) {
-  if (event.code === 'Enter') {
+  if (event.code === "Enter") {
     event.preventDefault();
   }
 }
 
 /* Starts a clock and uses its current time. */
-useNowCheckbox.addEventListener('change', () => {
+useNowCheckbox.addEventListener("change", () => {
   clearInterval(updateDateInterval);
   if (useNowCheckbox.checked) {
     dateInput.value = "";
@@ -70,7 +70,7 @@ useNowCheckbox.addEventListener('change', () => {
 });
 
 /* Use a manually input date value. */
-dateInput.addEventListener('input', () => {
+dateInput.addEventListener("input", () => {
   useNowCheckbox.checked = false;
   clearInterval(updateDateInterval);
   date = new Date(dateInput.value);
@@ -78,50 +78,53 @@ dateInput.addEventListener('input', () => {
 });
 
 /* Handle locale input. */
-localesInput.addEventListener('input', () => {
+localesInput.addEventListener("input", () => {
   try {
     if (localesInput.value) {
-      const inputLocales = localesInput.value?.split(/,\s*/).filter(s => s !== '');
-      const supportedLocales = Intl.DateTimeFormat.supportedLocalesOf(inputLocales);
+      const inputLocales = localesInput.value
+        ?.split(/,\s*/)
+        .filter((s) => s !== "");
+      const supportedLocales =
+        Intl.DateTimeFormat.supportedLocalesOf(inputLocales);
       if (supportedLocales.length) {
         locales = supportedLocales;
-        localesInput.classList.remove('error');
+        localesInput.classList.remove("error");
       }
     } else {
-      localesInput.classList.remove('error');
+      localesInput.classList.remove("error");
     }
   } catch {
-    localesInput.classList.add('error');
+    localesInput.classList.add("error");
   }
   updateOutput();
 });
 
 /* Handle time zone input. */
-timeZoneInput.addEventListener('input', () => {
+timeZoneInput.addEventListener("input", () => {
   try {
     if (timeZoneInput.value) {
       Intl.DateTimeFormat(undefined, { timeZone: timeZoneInput.value });
       timeZone = timeZoneInput.value;
-      timeZoneInput.classList.remove('error');
+      timeZoneInput.classList.remove("error");
     } else {
-      timeZoneInput.classList.remove('error');
+      timeZoneInput.classList.remove("error");
     }
   } catch {
-    timeZoneInput.classList.add('error');
+    timeZoneInput.classList.add("error");
   }
   updateOutput();
 });
 
 // Prevent form submission.
-localesInput.addEventListener('keydown', preventSubmitOnEnter);
-timeZoneInput.addEventListener('keydown', preventSubmitOnEnter);
+localesInput.addEventListener("keydown", preventSubmitOnEnter);
+timeZoneInput.addEventListener("keydown", preventSubmitOnEnter);
 
 /* Handle radio input. */
-optionsForm.addEventListener('radioGroupInput', () => {
+optionsForm.addEventListener("radioGroupInput", (event) => {
   if (STYLE_PROPERTIES.includes(event.detail.name)) {
-    clearProperties.apply(null, FORMAT_PROPERTIES);
+    clearProperties(...FORMAT_PROPERTIES);
   } else if (FORMAT_PROPERTIES.includes(event.detail.name)) {
-    clearProperties.apply(null, STYLE_PROPERTIES);
+    clearProperties(...STYLE_PROPERTIES);
   }
 
   setFormatOption(event.detail.name, event.detail.value);
@@ -131,11 +134,11 @@ optionsForm.addEventListener('radioGroupInput', () => {
 // Initialize values
 formatOptions = {};
 date = new Date();
-localesInput.setAttribute('placeholder', `(e.g. "${navigator.languages[0]}")`);
+localesInput.setAttribute("placeholder", `(e.g. "${navigator.languages[0]}")`);
 
 // Need to manually create a date whose local time in UTC is the same as the current time in the local time zone.
 const offsetDate = new Date();
-offsetDate.setTime(date.getTime() - (date.getTimezoneOffset() * 60 * 1000));
+offsetDate.setTime(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
 dateInput.value = offsetDate.toISOString().substring(0, 19);
 
 updateOutput();
--- radio-group.js
+++ radio-group.js
@@ -1,5 +1,5 @@
-const radioGroupTemplate = document.getElementById('radio-group-template');
-const radioInputTemplate = document.getElementById('radio-input-template');
+const radioGroupTemplate = document.getElementById("radio-group-template");
+const radioInputTemplate = document.getElementById("radio-input-template");
 
 /**
  * A simple vanilla web component to handle radio groups.
@@ -16,69 +16,69 @@ const radioInputTemplate = document.getElementById('radio-input-template');
  */
 class RadioGroup extends HTMLElement {
   get name() {
-    return this.getAttribute('name');
+    return this.getAttribute("name");
   }
 
   get values() {
-    return this.dataset.values.split(',');
+    return this.dataset.values.split(",");
   }
 
   get header() {
-    return this.shadowRoot.querySelector('#header');
+    return this.shadowRoot.querySelector("#header");
   }
 
   get clearButton() {
-    return this.shadowRoot.querySelector('#clearButton');
+    return this.shadowRoot.querySelector("#clearButton");
   }
 
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({mode: 'open'});
-    shadowRoot.appendChild(radioGroupTemplate.content.cloneNode(true));
   }
 
   connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(radioGroupTemplate.content.cloneNode(true));
+
     this.header.innerText = this.name;
-    this.clearButton.addEventListener('click', () => {
+    this.clearButton.addEventListener("click", () => {
       this.clear();
       this.update(undefined);
     });
 
-    this.values.forEach(value => {
+    this.values.forEach((value) => {
       const radioNode = this.createRadioInputNode(value);
-      this.shadowRoot.appendChild(radioNode);
+      shadowRoot.appendChild(radioNode);
     });
   }
 
   update(value) {
     this.dispatchEvent(
-      new CustomEvent(
-        'radioGroupInput',
-        {
-          bubbles: true,
-          detail: { name: this.name, value: value }
-        }
-      )
+      new CustomEvent("radioGroupInput", {
+        bubbles: true,
+        detail: { name: this.name, value: value },
+      }),
     );
   }
 
-  clear(shouldUpdate = false) {
-    this.removeAttribute('selected');
-    this.shadowRoot.querySelectorAll('input').forEach(input => input.checked = false);
+  clear() {
+    this.removeAttribute("selected");
+    this.shadowRoot
+      .querySelectorAll("input")
+      .forEach((input) => (input.checked = false));
   }
 
   createRadioInputNode(value) {
     const radioNode = radioInputTemplate.content.cloneNode(true);
-    const radioInput = radioNode.querySelector('input');
-    radioInput.setAttribute('name', this.name);
-    radioInput.setAttribute('value', value);
-    radioInput.addEventListener('input', () => {
-      this.setAttribute('selected', '');
+    const radioInput = radioNode.querySelector("input");
+    radioInput.setAttribute("name", this.name);
+    radioInput.setAttribute("value", value);
+    radioInput.addEventListener("input", () => {
+      this.setAttribute("selected", "");
       this.update(value);
     });
-    radioNode.querySelector('.radio-label').innerText = value;
+    radioNode.querySelector(".radio-label").innerText = value;
     return radioNode;
   }
 }
 
-customElements.define('radio-group', RadioGroup);
+customElements.define("radio-group", RadioGroup);
